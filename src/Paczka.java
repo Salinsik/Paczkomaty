a597400c6632b891d5305e8864499b806e43b3b3
salins <kontakt@salins.pl>
2019-05-11 16:51:23 +0200

    Ready

--- src/Paczka.java
+++ src/Paczka.java
@@ -1,2 +1,35 @@
 public class Paczka {
+    private String kod;
+    private int waga;
+
+    public Paczka() {
+        this.setKod("0");
+        this.setWaga(0);
+    }
+
+    public Paczka(String kod, int waga) {
+        this.setKod(kod);
+        this.setWaga(waga);
+    }
+
+
+    public String getKod() {
+        return kod;
+    }
+
+    public void setKod(String kod) {
+        if (kod != null && kod.matches("^   [A-Z]{3}[A-Z0-9]{5}$")) {
+            this.kod = kod;
+        } else {
+            throw new IllegalArgumentException("Nieprawidłowe kod.");
+        }
+    }
+
+    public int getWaga() {
+        return waga;
+    }
+
+    public void setWaga(int waga) {
+        this.waga = waga;
+    }
 }
--- src/Paczkomat.java
+++ src/Paczkomat.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Paczkomat {
+    public static void main(String[] args) {
+        Paczkomat paczkomat = new Paczkomat();
+        Paczka paczka2 = new Paczka("LLC10255",2);
+
+        paczkomat.dodajPaczke(paczka2);
+        paczkomat.dodajPaczke(new Paczka("LBC10255",15));
+        paczkomat.wyswietlZawartosc(paczkomat);
+    }
+
+    List<Paczka> paczki = new ArrayList();
+
+    public void dodajPaczke(Paczka paczka) {
+        paczki.add(paczka);
+    }
+
+    public void wyswietlZawartosc(Paczkomat paczkomat) {
+        for (Paczka paczka : paczki) {
+            System.out.println("Kod: " + paczka.getKod() + ", Waga: " + paczka.getWaga());
+        }
+    }
+
 }
